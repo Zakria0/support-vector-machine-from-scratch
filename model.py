61cc987e8ca33f518dea926803452cd4c5aaae41
Oulhadj Zakaria <69174113+Zakria0@users.noreply.github.com>
2026-09-24 15:43:18 +0100

    Step 10: predict_labels

--- model.py
+++ model.py
@@ -71,8 +71,12 @@ def train_svm(x, y, learning_rate, reg_lambda, n_epochs):
         params = apply_update(params, grads, learning_rate)
     return params
 
-# Step 10 - predict_labels (not yet solved)
-# TODO: implement
+# Step 10 - predict_labels
+import numpy as np
+
+def predict_labels(x, params):
+    scores = compute_scores(x, params)
+    return predict_from_scores(scores)
 
 # Step 11 - accuracy_score (not yet solved)
 # TODO: implement
