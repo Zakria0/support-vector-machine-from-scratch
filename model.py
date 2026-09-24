8641e3df834ba20ad0ce60aaf119db8294bae004
Oulhadj Zakaria <69174113+Zakria0@users.noreply.github.com>
2026-09-24 15:11:30 +0100

    Step 6: svm_objective

--- model.py
+++ model.py
@@ -39,8 +39,9 @@ def predict_from_scores(scores):
 def hinge_loss_example(score, y):
     return np.clip(1 - y * score, a_min=0, a_max=None)
 
-# Step 6 - svm_objective (not yet solved)
-# TODO: implement
+# Step 6 - svm_objective
+def svm_objective(x, y, params, reg_lambda):
+    return float(np.mean(hinge_loss_example(compute_scores(x, params), y)) + reg_lambda * (params['w'] @ params['w']))
 
 # Step 7 - compute_gradients (not yet solved)
 # TODO: implement
